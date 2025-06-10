c419b35e5dace0eef35946d9cf09823d2d338dae
VishalAG67 <vishalplc67@gmail.com>
2025-06-10 10:39:48 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (21.98%) - LeetSync

--- 1635-number-of-good-pairs/number-of-good-pairs.cpp
+++ 1635-number-of-good-pairs/number-of-good-pairs.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     int numIdenticalPairs(vector<int>& nums) {
+        unordered_map<int,int> freq;
         int count = 0;
-        int n = nums.size(); 
-        
-        for (int i = 0; i < n - 1; i++) {  
-            for (int j = i + 1; j < n; j++) {
-                if (nums[i] == nums[j]) {  
-                    count++;
-                }
-            }
+         
+        for(int num : nums){
+            count += freq[num];
+            freq[num]++;
         }
-        return count;
+      return count;
+        
     }
 };
